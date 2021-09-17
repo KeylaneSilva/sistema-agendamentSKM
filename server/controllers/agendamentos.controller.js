8bdd1a64e940cad7354367e69079130c43b554e5
Keylane <keylanessilva07@gmail.com>
2021-09-17 01:39:36 -0300

    controller agendamento

---
+++ server/controllers/agendamentos.controller.js
@@ -0,0 +1,46 @@
+const Agendamentos = require('../models/agendamento.model');
+
+
+module.exports = {
+    async index(req ,res){
+        const agend = await Agendamentos.find();
+        return res.json(agend);
+    },
+    async create(req, res){
+        const { nome_cliente, data_agendamento, horario_agendamento, nome_procedimento, valor, observacao_agendamento, foiAtendida } = req.body;
+
+        let data = {}
+        let agend = await Agendamentos.findOne({nome_cliente});
+
+        if(!agend){
+           data = { nome_cliente, data_agendamento, horario_agendamento, nome_procedimento, valor, observacao_agendamento, foiAtendida}
+
+           agend = await Agendamentos.create(data);
+
+           return res.status(200).json(agend);
+        }else{
+            return res.status(500).json(agend);
+        }
+
+    },
+
+    async details(req, res){
+        const { _id } = req.params;
+        const agend = await Agendamentos.findById({_id});
+        return res.json(agend);
+    },
+
+    async delete(req, res){
+        const { _id } = req.params;
+        const agend = await Agendamentos.findByIdAndDelete({_id});
+        return res.json(agend);
+    },
+    async update(req, res){
+        const { _id, nome_cliente, data_agendamento, horario_agendamento, nome_procedimento, valor, observacao_agendamento, foiAtendida } = req.body;
+        
+        const data = {_id, nome_cliente, data_agendamento, horario_agendamento, nome_procedimento, valor, observacao_agendamento, foiAtendida};
+
+        const agend = await Agendamentos.findByIdAndUpdate({_id}, data, {new: true});
+        return res.json(agend)
+    }
+}
