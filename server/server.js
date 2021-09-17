8a3677ac3888b0c590a77b717bf5b7f282a28765
Keylane <keylanessilva07@gmail.com>
2021-09-17 01:39:10 -0300

    server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { env } = require('process');
+const cookieParser = require('cookie-parser');
+const app = express();
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 8997;
+
+// CONEXÃO MONGOOSE
+mongoose.connect('mongodb://localhost:27017/system-agendamento', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+},(err) => {
+    if(err){
+        console.log('Houve um erro: '+err)
+    }else{
+        console.log('MongoDB CONECTADO com sucesso!')
+    }
+})
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
