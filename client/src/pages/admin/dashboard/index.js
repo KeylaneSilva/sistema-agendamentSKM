efd0408da789534a5ff28c46fed781fcf5b4c23a
Keylane <keylanessilva07@gmail.com>
2021-10-14 09:54:16 -0300

    adicionado pages

--- client/src/pages/admin/dashboard/index.js
+++ client/src/pages/admin/dashboard/index.js
@@ -1,7 +1,58 @@
-export default function Dashboard(){
-    return(
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import MenuAdmin from '../../../components/menu-admin';
+import Footer from '../../../components/footer-admin';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+  
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}> 
+    <MenuAdmin title={'DASHBOARD'}/>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            {/* <img src={ImgAdmin} /> */}
+          </Grid>
+          <Box pt={4}>
+            <Footer />
+          </Box>
+        </Container>
+      </main>
+    </div>
+  );
 }
